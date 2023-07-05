8181945a51155b2b07aeafea669788200cae60b2
Rajneesh Dubey <dubeyrajneesh786@gmail.com>
2023-07-05 09:15:40 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import HomePage from './Components/Pages/HomePage';
 import AboutPage from './Components/Pages/AboutPage';
-import ContactPage from './Components/ContactPage';
+import ContactPage from './Components/Pages/ContactPage';
 import Navbar from './Components/Layout/Navbar';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom' ;
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './Components/Pages/PageNotFound';
 import Adduser from './Components/Users/Adduser';
 import EditUser from './Components/Users/EditUser';
@@ -17,25 +17,24 @@ import Viewuser from './Components/Users/Viewuser';
 function App() {
   return (
     <Router>
-    <div>
-      <Navbar/>
+      <div>
+        <Navbar />
 
-      <Routes>
-        <Route exact path="/" element={<HomePage/>} />
-        <Route exact path="/About" element={<AboutPage/>} />
-        <Route exact path="/Contact" element={<ContactPage/>} />
-        <Route exact path="/Adduser/Add" element={<Adduser/>} />
-        <Route exact path="/EditUser/Edit/:UserId" element={<EditUser/>} />
-        <Route exact path="/Viewuser/View/:UserId" element={<Viewuser/>} />
-        <Route path="*" element = {<PageNotFound/>} />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/About" element={<AboutPage />} />
+          <Route exact path="/Contact" element={<ContactPage />} />
+          <Route exact path="/Adduser/Add" element={<Adduser />} />
+          <Route exact path="/EditUser/Edit/:UserId" element={<EditUser />} />
+          <Route exact path="/Viewuser/View/:UserId" element={<Viewuser />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
-     
 
-      
-    </div>
+
+
+      </div>
     </Router>
   );
 }
 
 export default App;
- 
